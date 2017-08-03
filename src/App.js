070f47ac47271dd4ca795f59e20e91d39332f763
liahwallace1 <liahwallace1@gmail.com>
2017-08-03 13:23:14 -0400

    create-react-app and added initial state

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      fetchingData: true,
+      weatherData: {}
+    }
+  }
+
+  render() {
+    const {fetchingData} = this.state
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          {
+            fetchingData ?
+            <img src={logo} className="App-logo" alt="logo" />
+            :
+            <h1>Weather Data</h1>
+          }
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
