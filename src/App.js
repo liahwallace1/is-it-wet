2283a0a623ad894ea9a2e85894675bb1cab6e04b
liahwallace1 <liahwallace1@gmail.com>
2017-08-03 14:47:57 -0400

    added .env for API key and APIURL constant

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const APIURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/`
+
 class App extends Component {
   constructor() {
     super()
