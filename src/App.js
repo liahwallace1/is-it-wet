e935f5d40c564ce056f00a45ef7b391b818f3c06
liahwallace1 <liahwallace1@gmail.com>
2017-08-04 14:30:14 -0400

    Edited css for header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import header from './StockSnap_C3E239A4B9.jpg';
 import './App.css';
 import './bootstrap.min.css';
-import { Col } from 'react-bootstrap';
+import { Jumbotron, Col } from 'react-bootstrap';
 
 const APIURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/`
 
@@ -21,13 +21,13 @@ class App extends Component {
     const {fetchingData} = this.state
     return (
       <div className="App">
-        <div className="App-header">
+        <Jumbotron className="App-header">
           <Col className="header-container" xs={8} md={4} xsOffset={2} mdOffset={4}>
-            <h2>Is it wet? 💦</h2>
+            <h1>Is it wet? 💦</h1>
             <br />
             <p>Find current forecast AND past weather data to determine if conditions are wet!</p>
           </Col>
-        </div>
+        </Jumbotron>
         <div className="App-intro">
           {
             fetchingData ?
