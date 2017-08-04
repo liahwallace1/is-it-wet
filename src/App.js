9b4e8c09586547c65549f3f1be7426b5097f425c
liahwallace1 <liahwallace1@gmail.com>
2017-08-04 14:13:54 -0400

    added React Bootstrap, bootstrap css files, and some styling to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import header from './StockSnap_C3E239A4B9.jpg';
 import './App.css';
+import './bootstrap.min.css';
+import { Col } from 'react-bootstrap';
 
 const APIURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}/`
 
@@ -9,7 +12,7 @@ class App extends Component {
     super()
 
     this.state = {
-      fetchingData: true,
+      fetchingData: false,
       weatherData: {}
     }
   }
@@ -19,17 +22,20 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-
-          <h2>Welcome to React</h2>
+          <Col className="header-container" xs={8} md={4} xsOffset={2} mdOffset={4}>
+            <h2>Is it wet? 💦</h2>
+            <br />
+            <p>Find current forecast AND past weather data to determine if conditions are wet!</p>
+          </Col>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           {
             fetchingData ?
             <img src={logo} className="App-logo" alt="logo" />
             :
-            <h1>Weather Data</h1>
+            <form><label>Search location: </label><input type='text' placeholder="zip code or city" /><input type="submit" /></form>
           }
-        </p>
+        </div>
       </div>
     );
   }
