2761f377311bd955cfa0e3881f35d2bd1a8f20bb
liahwallace1 <liahwallace1@gmail.com>
2017-08-05 09:28:47 -0400

    added a map component (copied from what's for lunch

---
+++ src/containers/Map.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class Map extends Component {
+
+  // componentDidMount() {
+  //   const position = {lat: this.props.restaurant.latitude, lng: this.props.restaurant.longitude}
+  //   const map = new this.props.google.maps.Map(this.refs.map, {
+  //     center: position,
+  //     zoom: 14
+  //   })
+  //   new this.props.google.maps.Marker({
+  //     map: map,
+  //     position: position
+  //   })
+  //   window.scrollTo(0,document.body.scrollHeight)
+  // }
+
+ render() {
+   const style = {
+     width: `600px`,
+     height: '430px'
+   }
+
+   return (
+     <div ref='map' className="google-map" style={style}>
+      Loading Map
+    </div>
+   )
+ }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    location: state.location,
+  }
+}
+
+
+export const ConnectedMap = connect(mapStateToProps)(Map)
