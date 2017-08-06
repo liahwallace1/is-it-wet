5d22be1b29aef56d033217d801c79a394b858002
liahwallace1 <liahwallace1@gmail.com>
2017-08-06 15:47:46 -0400

    updated map key info

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {GoogleApiWrapper} from 'google-maps-react';
+import {ConnectedSearchForm} from './SearchForm';
+import {ConnectedMap} from './Map';
+
+class MapContainer extends Component {
+
+  render() {
+
+    const style = {
+      width: '100vw',
+      height: '500px',
+    }
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div className='map-container' style={style}>
+        <ConnectedSearchForm />
+        <ConnectedMap google={this.props.google} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    location: state.location,
+  }
+}
+
+export const ConnectedMapContainer = connect(mapStateToProps)(GoogleApiWrapper({
+  apiKey: `${process.env.REACT_APP_GOOGLE_GEOCODING_KEY}`, version: '3.27'
+})(MapContainer))
