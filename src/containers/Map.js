03c090b6695996bf7013cc3d2eedfbdd578e23ad
liahwallace1 <liahwallace1@gmail.com>
2017-08-06 16:09:55 -0400

    set up function return initial map at current location

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import logo from '../logo.svg';
 
 class Map extends Component {
 
-  // componentDidMount() {
-  //   const position = {lat: this.props.restaurant.latitude, lng: this.props.restaurant.longitude}
-  //   const map = new this.props.google.maps.Map(this.refs.map, {
-  //     center: position,
-  //     zoom: 14
-  //   })
-  //   new this.props.google.maps.Marker({
-  //     map: map,
-  //     position: position
-  //   })
-  //   window.scrollTo(0,document.body.scrollHeight)
-  // }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(currentLoc => {
+      debugger
+      const position = currentLoc !== undefined ? {lat: currentLoc.coords.latitude, lng: currentLoc.coords.longitude} : {lat: 32.765914, lng: -79.899101}
+      console.log(position)
+      const map = new this.props.google.maps.Map(this.refs.map, {
+        center: position,
+        zoom: 11
+      })
+      new this.props.google.maps.Marker({
+        map: map,
+        position: position
+      })
+      window.scrollTo(0,document.body.scrollHeight)
+    })
+  }
 
  render() {
    const style = {
@@ -24,7 +29,7 @@ class Map extends Component {
 
    return (
      <div ref='map' className="google-map" style={style}>
-      Loading Map
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
    )
  }
